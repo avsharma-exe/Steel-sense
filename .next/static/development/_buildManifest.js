a8e2bd2ec8d398915096953669677d023d5ae9f1
Naushad Dhun <naushaddhun@gmail.com>
2022-06-07 22:30:23 +0530

    removed .next

--- .next/static/development/_buildManifest.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/login":["static\u002Fchunks\u002Fpages\u002Flogin.js"],sortedPages:["\u002F_app","\u002F_error","\u002Flogin"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/acl":["static\u002Fchunks\u002Fpages\u002Facl.js"],"/home":["static\u002Fchunks\u002Fpages\u002Fhome.js"],sortedPages:["\u002F_app","\u002F_error","\u002Facl","\u002Fhome"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 55e4fda8275e2ef06a2a8fe7a5c9f0ec0fb52205
