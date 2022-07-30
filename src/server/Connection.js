d1094a0077b8b35bb65e6ce0752597134a189ce4
unknown <codeblooded018@gmail.com>
2022-07-30 23:49:59 +0530

    Hosting

--- src/server/Connection.js
+++ src/server/Connection.js
@@ -1,23 +1,38 @@
-const mysql = require('mysql')
+// const mysql = require('mysql')
+import mysql from 'serverless-mysql'
 
 import mig_db from './database/mig_db'
 
 // declaring database connection
-const db = mysql.createPool(mig_db.hosted)
+// const db = mysql.createPool(mig_db.hosted)
 
-db.connect();
+// db.connect();
 
 // execute query function
+// export default async function executeQuery({ query, values }) {
+//     return new Promise((resolve, reject) => {
+//         console.log(query)
+//         db.query(query, values, (error, rows, fields) => {
+//             if (error) {
+//                 reject(error);
+//             }
+//             // console.log(rows);
+//             resolve(rows);
+//             db.end();
+//         })
+//     });
+// }
+
+const db = mysql({
+  config: mig_db.hosted
+})
+
 export default async function executeQuery({ query, values }) {
-    return new Promise((resolve, reject) => {
-        console.log(query)
-        db.query(query, values, (error, rows, fields) => {
-            if (error) {
-                reject(error);
-            } 
-            // console.log(rows);
-            resolve(rows);
-            db.end();
-        })
-    });
+  try {
+    const results = await db.query(query, values)
+    await db.end()
+    return results
+  } catch (error) {
+    return { error }
+  }
 }
