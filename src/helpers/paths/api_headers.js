04b2ee4aeb81f9630c747f918ba7c72aa3c84399
unknown <codeblooded018@gmail.com>
2022-08-05 15:24:42 +0530

    Hosting

--- src/helpers/paths/api_headers.js
+++ src/helpers/paths/api_headers.js
@@ -1,5 +1,5 @@
 module.exports = {
-  
-  // api_secure_header: 'https://steelsense.vercel.app/api'
-  api_secure_header: 'http://localhost:3000/api'
+
+  api_secure_header: 'https://steelsense.vercel.app/api'
+  // api_secure_header: 'http://localhost:3000/api'
 }
