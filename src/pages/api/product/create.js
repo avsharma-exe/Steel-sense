a4fa9bfdc08e17b4e5419ac3ea966aac8b268095
unknown <codeblooded018@gmail.com>
2022-10-03 20:28:38 +0530

    testing create API

--- src/pages/api/product/create.js
+++ src/pages/api/product/create.js
@@ -47,6 +47,7 @@ export default async function handler(req, res) {
         LastStock: stock.openingStock,
         LowStockLimit: stock.LowStockLimit,
         MaxStockLimit: stock.MaxStockLimit,
+        Co_ID: body.userDetails.Co_ID,
         Div_ID: division,
         ...userfeilds
       }
